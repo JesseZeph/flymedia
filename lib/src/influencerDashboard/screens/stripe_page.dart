920205755476e6890bcbaa50362161cda930c861
Jesse Josiah <jessezephyr@gmail.com>
2024-02-23 19:20:03 +0100

    update

--- lib/src/influencerDashboard/screens/stripe_page.dart
+++ lib/src/influencerDashboard/screens/stripe_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flymedia_app/providers/payment_provider.dart';
 import 'package:flymedia_app/src/authentication/forgotpassword/screens/checkemail.dart';
 import 'package:flymedia_app/src/clientdashboard/contracts/payment_success.dart';
+import 'package:flymedia_app/utils/extensions/context_extension.dart';
 import 'package:flymedia_app/utils/widgets/alert_loader.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -70,12 +71,7 @@ class StripeWebView extends StatelessWidget {
                         (state) {
                           if (state == PaymentState.error) {
                             Navigator.pop(context);
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const CancelPage(),
-                              ),
-                            );
+                            context.showError('Payment not successful');
                           }
                         },
                       );
@@ -90,34 +86,6 @@ class StripeWebView extends StatelessWidget {
                 },
                 onWebResourceError: (WebResourceError error) {},
               ),
-      ),
-    );
-  }
-}
-
-class CancelPage extends StatelessWidget {
-  const CancelPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 120.w),
-              child: ImageWithTextWidget(
-                assetImage: Image.asset(
-                  'assets/images/error.png',
-                ),
-                headerText: 'Payment Received',
-                subText:
-                    'Your transaction wasn\'nt successful, please try again later',
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
